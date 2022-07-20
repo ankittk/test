07c1e8ba818aea022b44b35919cf78496978bc15
ankittk <ankit.kumar@getweave.com>
2022-07-21 02:00:39 +0530

    test

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"time"
-
+	
 	"weavelab.xyz/deployer-resource-sync/pkg/resource"
+
 	"weavelab.xyz/monorail/shared/wlib/werror"
+
 	"weavelab.xyz/wstore/pkg/clusters"
 )
 
